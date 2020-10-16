23eb078f166aed2ace7844041affb684b09312d7
xdawxd <xrealz110@gmail.com>
2020-10-16 23:04:26 +0200

    Added project

---
+++ 14. CSV, JSON and API/weatherAPI.py
@@ -0,0 +1,29 @@
+# weatherAPI.py - Displays weather in the localization you passed inside a terminal.
+
+import json
+import requests
+import sys
+import pprint
+
+if len(sys.argv) < 2:
+    print('Uzycie: weatherAPI.py lokalizacja')
+    sys.exit()
+location = ' '.join(sys.argv[1:])
+
+url = 'http://api.openweathermap.org/data/2.5/forecast?q=%s&appid=8691c9e9ecc96acd0694b0760f069554&lang=pl' % location
+response = requests.get(url)
+response.raise_for_status()
+
+weatherData = json.loads(response.text)
+
+w = weatherData['list']
+
+print('Aktualna pogoda w %s: ' % location)
+print()
+print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+print()
+print('Jutro: ')
+print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+print()
+print('Pojutrze: ')
+print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
